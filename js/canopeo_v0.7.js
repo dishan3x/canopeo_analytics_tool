4a737c662e809a7c523db408fb5d779aa431761e
dishan <dishan@ksu.edu>
2020-02-24 03:01:21 -0600

    add a console log test

--- js/canopeo_v0.7.js
+++ js/canopeo_v0.7.js
@@ -281,15 +281,42 @@ function setup() {
 
   // Or with jQuery
 
- 
+  var output = document.createElement('pre');
+  document.body.appendChild(output);
+  
+  // Reference to native method(s)
+  var oldLog = console.log;
+  
+  console.log = function( ...items ) {
+  
+      // Call native method first
+      oldLog.apply(this,items);
+  
+      // Use JSON to transform objects, all others display normally
+      items.forEach( (item,i)=>{
+          items[i] = (typeof item === 'object' ? JSON.stringify(item,null,4) : item);
+      });
+      output.innerHTML += items.join(' ') + '<br />';
+  
+  };
 
 
+}  // End of setup **************************88
+
+
+
+// You could even allow Javascript input...
+function consoleInput( data ) {
+    // Print it to console as typed
+    console.log( data + '<br />' );
+    try {
+        console.log( eval( data ) );
+    } catch (e) {
+        console.log( e.stack );
+    }
 }
 
-var setCanvasSize = function() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    }
+
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
