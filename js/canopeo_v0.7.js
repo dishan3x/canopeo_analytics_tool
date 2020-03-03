a1100fe52ad8d4954a74afd572b3a6d62725bdc3
Dishan Nahitiya <dishan@users.campus>
2020-03-03 15:27:04 -0600

    changes in spelling

--- js/canopeo_v0.7.js
+++ js/canopeo_v0.7.js
@@ -747,13 +747,14 @@ function getETOValue(location,weather) {
 
 // Using haversine function 
 // adaptation  http://www.movable-type.co.uk/scripts/latlong.html
- function distance(lat1, lon1, lat2, lon2, unit) {
+// function distance(lat1, lon1, lat2, lon2, unit) {
+function distance(lat1, lon1, lat2, lon2, unit) {
 	if ((lat1 == lat2) && (lon1 == lon2)) {
 		return 0;
 	}
 	else {
 		var radlat1 = Math.PI * lat1/180;
-		var radlat2 = Math.PI * lat2/180;
+		var radlat2 = Math.PI * lat2/180; 
 		var theta = lon1-lon2;
 		var radtheta = Math.PI * theta/180;
 		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
@@ -763,8 +764,8 @@ function getETOValue(location,weather) {
 		dist = Math.acos(dist);
 		dist = dist * 180/Math.PI;
 		dist = dist * 60 * 1.1515;
-		if (unit=="K") { dist = dist * 1.609344 }
-		if (unit=="N") { dist = dist * 0.8684 }
+	//	if (unit=="K") { dist = dist * 1.609344 }
+	//	if (unit=="N") { dist = dist * 0.8684 }
 		return dist;
 	}
 }
@@ -781,7 +782,7 @@ function getMesonetStations(){
     new Promise(function(resolve, reject) {
         const timeout = setTimeout(function() {
             didTimeOut = true;
-            findNearestStation();
+            getMesonetStations();
             reject(new Error('Request timed out'));
         }, FETCH_TIMEOUT);
         
