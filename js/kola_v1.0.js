ce671191d1f0125b4ff0b4ef55c1fcfe3f2b51e4
dishan <dishan@ksu.edu>
2020-04-14 14:10:35 -0500

    Debugging statement added

--- js/kola_v1.0.js
+++ js/kola_v1.0.js
@@ -127,6 +127,26 @@ function setup() {
         }
     }
 
+    //testing ************************************
+    var output = document.createElement('pre');
+  document.body.appendChild(output);
+
+  // Reference to native method(s)
+  var oldLog = console.log;
+
+  console.log = function( ...items ) {
+
+      // Call native method first
+      oldLog.apply(this,items);
+
+      // Use JSON to transform objects, all others display normally
+      items.forEach( (item,i)=>{
+          items[i] = (typeof item === 'object' ? JSON.stringify(item,null,4) : item);
+      });
+      output.innerHTML += items.join(' ') + '<br />';
+
+  };
+
 }  // End setup()
 
 
