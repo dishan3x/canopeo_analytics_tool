e8271b442876df19aabc7e0e46670489ac384cf4
dishan <dishan@ksu.edu>
2020-04-21 03:25:07 -0500

    getCurrnet position check

--- js/kola_v1.0.js
+++ js/kola_v1.0.js
@@ -396,7 +396,7 @@ function altitudeToMeters(value, ref) {
 */
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(showPosition,showError);
+        navigator.geolocation.getCurrentPosition(showPosition,showError);
     } else {
         realtimeLatitude = null;
         realtimeLongitude = null;
